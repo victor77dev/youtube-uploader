575fc0d0f08534408efab29be27e325380eb0373
Victor Wu <victor.wu@zattoo.com>
2022-11-13 08:41:04 +0100

    Use gsi to login

--- test.js
+++ test.js
@@ -1,74 +1,26 @@
-var GoogleAuth;
-var SCOPE = 'https://www.googleapis.com/auth/youtube.force-ssl';
-function handleClientLoad() {
-    // Load the API's client and auth2 modules.
-    // Call the initClient function after the modules load.
-    gapi.load('client:auth2', initClient);
-}
+var client;
+var access_token;
+
+var SCOPE = 'https://www.googleapis.com/auth/youtube.readonly';
 
 function initClient() {
-    // In practice, your app can retrieve one or more discovery documents.
-    var discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest';
-
-    // Initialize the gapi.client object, which app uses to make API requests.
-    // Get API key and client ID from API Console.
-    // 'scope' field specifies space-delimited list of access scopes.
-    gapi.client.init({
-        'apiKey': process.env.YOUTUBE_API_KEY, 
-        'clientId': process.env.YOUTUBE_CLIENT_ID,
-        'discoveryDocs': [discoveryUrl],
-        'scope': SCOPE
-    }).then(function () {
-    GoogleAuth = gapi.auth2.getAuthInstance();
-
-    // Listen for sign-in state changes.
-    GoogleAuth.isSignedIn.listen(updateSigninStatus);
-
-    // Handle initial sign-in state. (Determine if user is already signed in.)
-    var user = GoogleAuth.currentUser.get();
-    setSigninStatus();
-
-    // Call handleAuthClick function when user clicks on
-    //      "Sign In/Authorize" button.
-    $('#sign-in-or-out-button').click(function() {
-        handleAuthClick();
-    });
-    $('#revoke-access-button').click(function() {
-        revokeAccess();
-    });
+    client = google.accounts.oauth2.initTokenClient({
+        client_id: process.env.YOUTUBE_CLIENT_ID,
+        scope: SCOPE,
+        callback: (tokenResponse) => {
+            access_token = tokenResponse.access_token;
+        },
     });
 }
-
-function handleAuthClick() {
-    if (GoogleAuth.isSignedIn.get()) {
-    // User is authorized and has clicked "Sign out" button.
-    GoogleAuth.signOut();
-    } else {
-    // User is not signed in. Start Google auth flow.
-    GoogleAuth.signIn();
-    }
+function getToken() {
+    client.requestAccessToken();
 }
-
-function revokeAccess() {
-    GoogleAuth.disconnect();
+function revokeToken() {
+    google.accounts.oauth2.revoke(access_token, () => {console.log('access token revoked')});
 }
-
-function setSigninStatus() {
-    var user = GoogleAuth.currentUser.get();
-    var isAuthorized = user.hasGrantedScopes(SCOPE);
-    if (isAuthorized) {
-    $('#sign-in-or-out-button').html('Sign out');
-    $('#revoke-access-button').css('display', 'inline-block');
-    $('#auth-status').html('You are currently signed in and have granted ' +
-        'access to this app.');
-    } else {
-    $('#sign-in-or-out-button').html('Sign In/Authorize');
-    $('#revoke-access-button').css('display', 'none');
-    $('#auth-status').html('You have not authorized this app or you are ' +
-        'signed out.');
-    }
-}
-
-function updateSigninStatus() {
-    setSigninStatus();
+function loadCalendar() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest');
+    xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+    xhr.send();
 }
